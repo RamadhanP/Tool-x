8066dfc244ee1a679d0e6273635e647eee104e08
ajirmdhn <45916317+ajirmdhn@users.noreply.github.com>
2019-06-04 00:27:41 +0700

    Update Tool-x.py

--- Tool-x.py
+++ Tool-x.py
@@ -99,7 +99,7 @@ HACKERPROlogo = """\033[0m
  | |  | | (_| | (__|   <  __/ |    | |   | | | (_) |
  |_|  |_|\__,_|\___|_|\_\___|_|    |_|   |_|  \___/ 
       
-            ~Tools for hacking by Muhammad Fazriansyah
+          Hacked tools author Muhammad Fazriansyah
 
 \033[91m"""
 def menu():
